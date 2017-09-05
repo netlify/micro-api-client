a9dffc6a9103587e46ebb95e00e777206eff8e10
Bryce Kahle <bkahle@gmail.com>
2017-09-05 16:25:42 -0400

    Add custom error types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import {getPagination} from './pagination';
 
+class HTTPError extends Error {
+  constructor(response) {
+    super(response.statusText);
+    this.status = response.status;
+    this.name = 'HTTPError';
+  }
+}
+
+class TextHTTPError extends HTTPError {
+  constructor(response, data) {
+    super(response);
+    this.data = data;
+    this.name = 'TextHTTPError';
+  }
+}
+
+class JSONHTTPError extends HTTPError {
+  constructor(response, json) {
+    super(response)
+    this.json = json;
+    this.name = 'JSONHTTPError';
+  }
+}
+
+export { HTTPError, TextError, JSONError };
+
 export default class API {
   constructor(apiURL) {
     this.apiURL = apiURL;
@@ -14,6 +40,10 @@ export default class API {
 
   parseJsonResponse(response) {
     return response.json().then((json) => {
+      if (!response.ok) {
+        return Promise.reject(new JSONHTTPError(response, json));
+      }
+
       const pagination = getPagination(response);
       return pagination ? {pagination, items: json} : json;
     });
@@ -22,17 +52,15 @@ export default class API {
   request(path, options = {}) {
     const headers = this.headers(options.headers || {});
     return fetch(this.apiURL + path, {...options, headers}).then((response) => {
-      if (!response.ok) {
-        return response.text().then(data => {
-          const err = new Error(data);
-          err.status = response.status;
-          return Promise.reject(err);
-        });
-      }
-
       const contentType = response.headers.get('Content-Type');
       if (contentType && contentType.match(/json/)) {
         return this.parseJsonResponse(response);
+      }
+
+      if (!response.ok) {
+        return response.text().then(data => {
+          return Promise.reject(new TextHTTPError(response, data));
+        });
       }
       return response.text().then((data) => {data});
     });
