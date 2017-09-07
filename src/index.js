ded94d4f58c8046a72dea4df56ea67f30b280384
Bryce Kahle <bkahle@gmail.com>
2017-09-07 17:55:04 -0400

    Fix exception when Error.captureStackTrace is undefined

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class HTTPError extends Error {
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor);
     } else { 
-      this.stack = (new Error(message)).stack; 
+      this.stack = (new Error(response.statusText)).stack; 
     }
     this.status = response.status;
   }
